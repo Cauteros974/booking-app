9e68ef43116a9cc992e68206b9f808fc75e00279
Cauteros974 <pusik6666@ukr.net>
2025-08-21 14:12:13 +0300

    commit

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -12,7 +12,41 @@ function BookingForm({ onBookingSubmit }) {
     };
 
     return( 
-        
+        <form onSubmit={handleSubmit} className="booking-form">
+            <div className="form-group">
+                <label htmlFor="check-in">Arrival date: </label>
+                <input 
+                    type="date"
+                    id="check-in"
+                    value={checkIn}
+                    onChange={(e) => setCheckIn(e.target.value)}
+                    required
+                />
+            </div>
+            <div className="form-group">
+            <label htmlFor="check-out">Дата выезда:</label>
+            <input
+                type="date"
+                id="check-out"
+                value={checkOut}
+                onChange={(e) => setCheckOut(e.target.value)}
+                required
+            />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="guests">Количество гостей:</label>
+                <input
+                    type="number"
+                    id="guests"
+                    value={guests}
+                    onChange={(e) => setGuests(e.target.value)}
+                    min="1"
+                    required
+                />
+            </div>
+            <butto type="submit" className="book-button">Make a reservation</butto>
+        </form>
     );
 }
 
